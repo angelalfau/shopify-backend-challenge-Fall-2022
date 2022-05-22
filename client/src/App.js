c58a2a86d42e6884b3a8102a9c86643994986214
Angel <angelalfau@gmail.com>
2022-05-22 14:00:22 -0500

    added deletion comments and undeletion

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
   const initialFormData = Object.freeze({
     title: "",
     amount: 1,
+    reason: "",
   });
 
   const [reload, setReload] = useState(false);
@@ -17,6 +18,9 @@ function App() {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const [readItems, setReadItems] = useState([]);
+  const [deleting, setDeleting] = useState(false);
+  const [deleteItem, setDeleteItem] = useState("");
+  const [restoreItems, setRestoreItems] = useState([]);
 
   const ItemRow = ({ item }) => {
     const handleEditClick = (e) => {
@@ -27,16 +31,10 @@ function App() {
     };
     const handleDeleteClick = async (e) => {
       e.preventDefault();
-      console.log(item._id);
-      await instance
-        .delete(`/delete-item/${item._id}`)
-        .then((res) => {
-          console.log(res);
-          setReload(!reload);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      console.log("deleting");
+      console.log(item);
+      setDeleting(true);
+      setDeleteItem(item);
     };
 
     return (
@@ -53,16 +51,62 @@ function App() {
     );
   };
 
-  const resetAll = () => {
-    updateFormData(initialFormData);
-    setReadItems([]);
-    setError("");
+  const RestoreRow = ({ item }) => {
+    const handleRestoreItem = async (e) => {
+      e.preventDefault();
+      console.log("restoring");
+      console.log(item);
+      setDeleting(true);
+      setDeleteItem(item);
+      instance
+        .post("restore-item", item)
+        .then((res) => {
+          console.log(res);
+          setReload(!reload);
+          setSuccess(`${item.title} restored`);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    const handleObliterateItem = async (e) => {
+      e.preventDefault();
+      console.log("restoring");
+      console.log(item);
+      instance
+        .post("obliterate-item", item)
+        .then((res) => {
+          console.log(res);
+          setReload(!reload);
+          setSuccess(`${item.title} has been obliterated`);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    return (
+      <tr>
+        <td>{item?.title}</td>
+        <td>{item?.reason}</td>
+        <td>
+          <Button onClick={handleRestoreItem}>Restore</Button>
+        </td>
+        <td>
+          <Button onClick={handleObliterateItem}>Obliterate</Button>
+        </td>
+      </tr>
+    );
   };
 
   useEffect(() => {
     if (selected === "read") {
       instance.get("/read-items").then((res) => {
         setReadItems(res.data);
+      });
+    }
+    if (selected === "restore") {
+      instance.get("/restore-items").then((res) => {
+        setRestoreItems(res.data);
       });
     }
   }, [selected, reload]);
@@ -77,24 +121,47 @@ function App() {
   const handleAddClick = (e) => {
     setSelected("add");
   };
-  const handleDeleteClick = (e) => {
-    setSelected("delete");
-  };
-  const handleUpdateClick = (e) => {
-    setSelected("update");
-  };
   const handleReadClick = (e) => {
     setSelected("read");
+  };
+  const handleRestoreClick = (e) => {
+    setSelected("restore");
   };
   const handleBackClick = (e) => {
     setSelected("");
     setError("");
     setSuccess("");
+    setDeleteItem("");
+    setEditItem("");
+    setDeleting(false);
+    setUpdating(false);
   };
+  const handleDeleteSubmit = (e) => {
+    e.preventDefault();
+    console.log("Item being deleted");
+    console.log(formData);
+    console.log(deleteItem);
 
+    instance
+      .post("/delete-item", { _id: deleteItem._id, reason: formData.reason })
+      // .delete(`/delete-item/${deleteItem._id}/${formData.reason}`)
+      .then((res) => {
+        console.log(res);
+        setDeleting(false);
+        setDeleteItem("");
+        setReload(!reload);
+        setSuccess(`${deleteItem.title} deleted`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   const itemList = readItems.map((item, i) => {
-    console.log(item);
     return <ItemRow key={i} item={item} />;
+  });
+
+  const RestoreList = restoreItems.map((item, i) => {
+    return <RestoreRow key={i} item={item} />;
   });
 
   const handleSubmit = (e) => {
@@ -130,7 +197,8 @@ function App() {
     instance
       .put("/update-item", formData)
       .then((res) => {
-        setSuccess(`${res.data.title} updated successfully`);
+        console.log(res.data);
+        setSuccess(`${editItem.title} updated successfully`);
         setError("");
         setUpdating(false);
         setEditItem("");
@@ -150,8 +218,9 @@ function App() {
         {success && <p>{success}</p>}
         {selected === "" ? (
           <div>
-            <Button onClick={handleAddClick}>Add Item</Button>
-            <Button onClick={handleReadClick}>Read Items</Button>
+            <Button onClick={handleAddClick}>Add New Item</Button>
+            <Button onClick={handleReadClick}>List of Active Items</Button>
+            <Button onClick={handleRestoreClick}>List of Deleted Items</Button>
           </div>
         ) : selected === "add" ? (
           <Form>
@@ -181,14 +250,50 @@ function App() {
             </Button>
           </Form>
         ) : selected === "read" ? (
+          <div>
+            <table cellPadding="0" cellSpacing="0">
+              <thead className="tbl-header">
+                <tr>
+                  <th>Item Title</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody className="tbl-content">{itemList}</tbody>
+            </table>
+            {deleting ? (
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Item Title: {deleteItem.title}</Form.Label>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Reason for deletion: </Form.Label>
+                  <Form.Control
+                    name="reason"
+                    type="text"
+                    required
+                    placeholder=""
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={handleDeleteSubmit}
+                >
+                  Delete Item
+                </Button>
+              </Form>
+            ) : null}
+          </div>
+        ) : selected === "restore" ? (
           <table cellPadding="0" cellSpacing="0">
             <thead className="tbl-header">
               <tr>
                 <th>Item Title</th>
-                <th>Amount</th>
+                <th>Reason for Deletion</th>
               </tr>
             </thead>
-            <tbody className="tbl-content">{itemList}</tbody>
+            <tbody className="tbl-content">{RestoreList}</tbody>
           </table>
         ) : null}
         {error !== "" && <p id="err-msg">Error: {error}</p>}
